67d0f4ec28aa6c3f19f923dba5278c4668f53db2
Sébastien D <sebastien.delahaye.contact@gmail.com>
2023-04-04 13:23:19 +0200

    ♻️ Clean code
    Code improvement
    Bug fixes

--- 3_code/public/assets/scripts/script.js
+++ 3_code/public/assets/scripts/script.js
@@ -1,15 +1,15 @@
 // Manage the sidebar
-const iconOpenSidebar = document.querySelector(".icon--open_sidebar");
-const iconCloseSidebar = document.querySelector(".icon--close_sidebar");
+const iconSidebarOpen = document.querySelector(".icon__sidebar--open");
+const iconSidebarClose = document.querySelector(".icon__sidebar--close");
 const main = document.querySelector(".main__block");
 const sidebar = document.querySelector(".sidebar__block");
 
 const manageSidebar = () => {
     sidebar.classList.toggle("show");
-    iconOpenSidebar.classList.toggle("show");
-    iconCloseSidebar.classList.toggle("show");
+    iconSidebarOpen.classList.toggle("show");
+    iconSidebarClose.classList.toggle("show");
     main.classList.toggle("hide");
 }
 
-iconOpenSidebar.addEventListener("click", manageSidebar);
-iconCloseSidebar.addEventListener("click", manageSidebar);
+iconSidebarOpen.addEventListener("click", manageSidebar);
+iconSidebarClose.addEventListener("click", manageSidebar);
