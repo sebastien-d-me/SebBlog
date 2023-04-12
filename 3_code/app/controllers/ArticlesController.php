230ee0696475dd63a4f7d7e12582a65f3ee3f13d
Sébastien D <sebastien.delahaye.contact@gmail.com>
2023-04-12 14:36:33 +0200

    ✨Add the comments to the article
    Add the comments to the article
    Fix bugs

--- 3_code/app/controllers/ArticlesController.php
+++ 3_code/app/controllers/ArticlesController.php
@@ -6,26 +6,16 @@ namespace App\Controllers;
 // Load
 use App\Controllers\DefaultController;
 use App\Models\Article;
+use App\Models\Comment;
 use App\Models\LoginCredentials;
 
 class ArticlesController extends DefaultController {
     // Show the articles
     function index() {
         $articles = Article::all();
-        $usernames = [];
-
-        foreach($articles as $article) {
-            $username = LoginCredentials::find($article->idMember);
-            $username = $username->getUsername();
-
-            $articlesList[] = [
-                "article" => $article,
-                "username" => $username
-            ];
-        }
 
         $this->twigRender("pages/articles.html.twig", [
-            "articlesList" => $articlesList
+            "articles" => $articles
         ]);
     }
 
@@ -33,12 +23,26 @@ class ArticlesController extends DefaultController {
     function showArticle() {
         $idArticle = $_GET["article"];
         $article = Article::where("idArticle", $idArticle)->first();
-        $username = LoginCredentials::find($article->idMember);
-        $username = $username->getUsername();
+        $writer = LoginCredentials::find($article->idMember);
+        $writer = $writer->getUsername();
+
+        $comments = Comment::where("idArticle", $idArticle)->get();
+
+        $commentsList = [];
+        foreach($comments as $comment) {
+            $user = LoginCredentials::find($comment->idMember);
+            $user = $user->getUsername();
+
+            $commentsList[] = [
+                "comment" => $comment,
+                "user" => $user
+            ];
+        }
 
         $this->twigRender("pages/article.html.twig", [
             "article" => $article,
-            "username" => $username
+            "commentsList" => $commentsList,
+            "writer" => $writer
         ]);
     }
 }
